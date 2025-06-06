7dabe5252b0868c4a9a9d2aa0757ba13febbd87d
Jean-Patrick-Alcolea <jeanpatrickalcolea@gmail.com>
2025-06-06 17:22:56 +0200

    Reglage KNN

--- app/views/Accueil.py
+++ app/views/Accueil.py
@@ -3,7 +3,7 @@ import pandas as pd
 from streamlit_cookies_controller import CookieController
 
 
-st.title("Cinéma Sénéchal")
+st.title("Creuse Toujours")
 
 # **************Initialization cookies et fonctions ************************
 
@@ -11,7 +11,7 @@ query_params = st.query_params
 
 
 def initialize():
-    return pd.read_parquet("data/df_similar.gzip")
+    return pd.read_parquet("data/movie_df.gzip")
 
 
 cookie_manager = CookieController()
--- app/views/Recommandations.py
+++ app/views/Recommandations.py
@@ -11,7 +11,7 @@ query_params = st.query_params
 
 
 def initialize():
-    return pd.read_parquet("data/st_df.gzip")
+    return pd.read_parquet("data/movie_df.gzip")
 
 
 cookie_manager = CookieController()
